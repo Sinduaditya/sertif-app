82b57253352d6e655e867df8aca16ecb27ffaf99
SinduAditya <nduujanadi51@gmail.com>
2024-11-28 07:43:36 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,13 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CertificateController;
 
-// Public routes
+
+Route::get('/certificates', [CertificateController::class, 'index'])->name('certificates.index');
+Route::get('/certificates/create', [CertificateController::class, 'create'])->name('certificates.create');
+Route::post('/certificates', [CertificateController::class, 'store'])->name('certificates.store');
 Route::get('/certificates/{certificateNumber}', [CertificateController::class, 'show'])->name('certificates.show');
 Route::get('/certificates/{certificateNumber}/download-qr', [CertificateController::class, 'downloadQrCode'])->name('certificates.downloadQr');
-
-
-    Route::get('/certificates', [CertificateController::class, 'index'])->name('certificates.index');
-    Route::get('/certificates/create', [CertificateController::class, 'create'])->name('certificates.create');
-    Route::post('/certificates', [CertificateController::class, 'store'])->name('certificates.store');
 
 
